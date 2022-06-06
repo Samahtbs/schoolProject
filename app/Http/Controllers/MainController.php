7b6be40f4ef670c5adc9ded10e9db2d16a21f5ab
Samahtbs <samah.tobasi@gmail.com>
2022-06-06 12:17:33 +0300

    Commit message

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\classTeacher;
 use App\Models\User;
+use App\Models\file;
 
 class MainController extends Controller
 {
@@ -13,12 +14,20 @@ class MainController extends Controller
         $class = classTeacher::find($id);
 
         $teacher = User::find($class['teacherId']);
+        $file = file::where('classid', $id)->get();
+        //return $file[0];
+
 
         $class['tname'] = $teacher['name'];
         $class['tphone'] = $teacher['phoneNumber'];
         $class['temail'] = $teacher['email'];
+        /*
+        $class['fname'] = $file[0]['FileName'];
+        $class['file'] = $file[0]['FileDecoded'];
+        $class['ftime'] = $file[0]['created_at'];
+        */
 
-        return view('classS', ['class' => $class]);
+        return view('classS', ['class' => $class, 'file' => $file]);
     }
 
     public function returnteacher($id)
--- resources/views/classS.blade.php
+++ resources/views/classS.blade.php
@@ -13,6 +13,15 @@
                     <h6>Class Name : <b>{{$class['ClassName']}}</b></h6>
                     <h6>Class Teacher : <a href="teacher/{{$class['teacherId']}}"><b>{{$class['tname']}}</b></a></h6>
                 </div>
+
+                @foreach($file as $f)
+                <div class="card-body">
+                    <h6>File name: <b>{{$f['FileName']}}</b></h6>
+                    <h6>File : <b>{{$f['FileDecoded']}}</b></h6>
+                    <h6>File uploaded time : <b>{{$f['created_at']}}</b></a></h6>
+                </div>
+                @endforeach
+
             </div>
         </div>
     </div>
